95921ba32c59d60c232ad94d2dc0cfe44869d60d
Thomas Ngo <tngo0508@gmail.com>
2018-08-12 19:49:31 -0700

    creating a settings class to modify the game during development

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Setting():
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        """Initialize the game's setting."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
