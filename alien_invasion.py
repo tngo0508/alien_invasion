ea69d0e6f0d9ca1e481c268993f02515f89059a5
Thomas Ngo <tngo0508@gmail.com>
2018-08-12 19:52:57 -0700

    make an instace of Settings and use it to access setting

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,15 @@
 """creating a pygame window and responding to user input"""
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     """running the game"""
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((
+        ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # Set the background color
@@ -21,7 +24,7 @@ def run_game():
                 sys.exit()
 
         # Redraw the screen during each pass through the loop
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # make the most recently drawn screen visible
         pygame.display.flip()
