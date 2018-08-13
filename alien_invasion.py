befeda3d2d23197eac3585310fc7f4a09ac9df54
Thomas Ngo <tngo0508@gmail.com>
2018-08-12 21:18:38 -0700

    allowing contiguous movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,6 +19,7 @@ def run_game():
     # Start the main loop
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -9,8 +9,10 @@ def check_events(ship):
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                # Move the ship to the right.
-                ship.rect.centerx += 1
+                ship.moving_right = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
