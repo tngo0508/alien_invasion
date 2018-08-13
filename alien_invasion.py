c0d507eef4aa731389c48db908c375017350547d
Thomas Ngo <tngo0508@gmail.com>
2018-08-12 23:12:25 -0700

    create bullet, store bullets in group

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 """creating a pygame window and responding to user input"""
 import pygame
+from pygame.sprite import Group
 import game_functions as gf
 from settings import Settings
 from ship import Ship
@@ -15,11 +16,14 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
-    # Start the main loop
+    # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
