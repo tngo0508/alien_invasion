2fcefc98baaab4353bb97553c5fbd5eb6d58f6df
Thomas Ngo <tngo0508@gmail.com>
2018-08-15 12:37:55 -0700

    Checking to see whether an alien has hit the edge

--- alien.py
+++ alien.py
@@ -25,7 +25,18 @@ class Alien(Sprite):
         self.screen.blit(self.image, self.rect)
 
 
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+        return False
+
+
     def update(self):
         """Move the alien right."""
-        self.x += self.ai_settings.alien_speed_factor
+        self.x += (self.ai_settings.alien_speed_factor *
+                   self.ai_settings.fleet_direction)
         self.rect.x = self.x
