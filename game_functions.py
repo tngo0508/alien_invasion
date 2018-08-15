6d1583657b178f2d9ea1ffd2d15d2cc1f25b78d8
Thomas Ngo <tngo0508@gmail.com>
2018-08-15 12:52:48 -0700

    Dropping the fleet and changing direction

--- game_functions.py
+++ game_functions.py
@@ -94,6 +94,21 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
+def check_fleet_edges(ai_settings, aliens):
+    """Respond appropriately if any aliens have reached an edge."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet and change the fleet's direction."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
 def update_screen(ai_settings, screen, ship, alien, bullets):
     """Update images on the screen and flip to the new screen."""
 
@@ -110,6 +125,10 @@ def update_screen(ai_settings, screen, ship, alien, bullets):
     pygame.display.flip()
 
 
-def update_aliens(aliens):
-    """Update the positions of all aliens in the fleet."""
+def update_aliens(ai_settings, aliens):
+    """
+    Check if the fleet is at an edge, and then
+    update the positions of all aliens in the fleet.
+    """
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
